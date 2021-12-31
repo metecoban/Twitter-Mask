306dbdc76ef400e0680d17c2645646b7697a2042
Mete Çoban <metecoban@192.168.1.33>
2021-12-31 16:27:53 +0300

    Fourth commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,79 +11,78 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("Twitter Mask"),
-      ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(top: 50, left: 50, right: 50),
-          child: Column(
-            children: [
-              SizedBox(height: 200, child: Image.asset("assets/logo.jpeg")),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Login',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your Email',
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  // suffixIcon: IconButton(
-                  //     onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your Password',
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text("Twitter Mask"),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.only(top: 50, left: 50, right: 50),
+              child: Column(
                 children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Login"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                      ),
+                  SizedBox(height: 200, child: Image.asset("assets/logo.jpeg")),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your Email',
                     ),
                   ),
                   const SizedBox(
-                    width: 10,
+                    height: 10,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/RegisterPage');
-                    },
-                    child: const Text("Register"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue.shade700,
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your Password',
                     ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Login"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/RegisterPage');
+                        },
+                        child: const Text("Register"),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue.shade700,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
